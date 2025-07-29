a181b8c27edaf31eff3b7b226bbc58b06971a0c4
Suraj-Mohapatra <suraj2895@gmail.com>
2025-07-29 20:37:35 +0530

    init

--- src/main/java/com/uglyeagle/JerseyEmbeddedServer.java
+++ src/main/java/com/uglyeagle/JerseyEmbeddedServer.java
@@ -1,34 +1,30 @@
 package com.uglyeagle;
 
-import java.io.IOException;
 import java.net.URI;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.eclipse.jetty.server.Server;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class JerseyEmbeddedServer {
 
-    public static final String BASE_URI = "http://localhost:8080/jersey/api/";
+    public static final String BASE_URI = "http://localhost:8080/";
+    //  tried to register http://localhost:8080/jersey/api
+    //  but in this way I could not register any path after http://localhost:8080
+    // hence access all the endpoints appending to http://localhost:8080
 
-    public static HttpServer startServer() {
-        final ResourceConfig rc = new ResourceConfig()
+    public static void main(String[] args) throws Exception {
+        final ResourceConfig config = new ResourceConfig()
                 .packages("com.uglyeagle.controller", "com.uglyeagle.exception");
 
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
+        Server server = JettyHttpContainerFactory.createServer(URI.create(BASE_URI), config);
 
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println("Jersey app started at " + BASE_URI);
-        System.out.println("Press Enter to stop the server...");
-        System.in.read();
-        server.shutdownNow();
+        try {
+            server.start();
+            System.out.println("Jersey app started at " + BASE_URI);
+            System.out.println("Press Enter to stop the server...");
+            System.in.read();
+        } finally {
+            server.stop();
+        }
     }
 }
-
-
-// jersey-container-simple-http
-// jersey-container-jetty-http
-// jersey-container-netty-http
-// can also be used instead of grizzly container
-// in such case com.uglyeagle.JerseyEmbeddedServer class implementation will vary w.r.t the api used
